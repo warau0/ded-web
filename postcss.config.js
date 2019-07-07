ecad0889f279462a13c82c42fe0b290626e3b871
warau0 <larswalwes@gmail.com>
2019-07-07 22:23:30 +0200

    0.12.1 Dependency bumps

--- postcss.config.js
+++ postcss.config.js
@@ -11,12 +11,6 @@ module.exports = {
       importFrom: 'src/ded-constants/constants.css',
     }),
     require('autoprefixer')({
-      browsers: [
-        '>1%',
-        'last 4 versions',
-        'Firefox ESR',
-        'not ie < 9',
-      ],
       flexbox: 'no-2009',
     }),
   ],
