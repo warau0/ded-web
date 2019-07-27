3c2a5e2d9b3482c7e8e825d193c2ff123f2b3d08
warau0 <larswalwes@gmail.com>
2019-07-27 12:07:31 +0200

    0.17.6 404 profile page

--- src/ded-routes/profile/index.jsx
+++ src/ded-routes/profile/index.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import { useApi } from 'ded-hooks';
 import { API, EVENT } from 'ded-constants';
-import { EventContext, LoginContext } from 'ded-context';
+import { EventContext, LoginContext, ThemeContext } from 'ded-context';
 import Gallery from 'ded-components/gallery';
 import Layout from 'ded-components/layout';
 import ProfileHeader from 'ded-components/profileHeader';
+import { cross as crossIcon } from 'ded-assets';
+
+import * as styles from './styles.pcss';
 
 const Profile = ({ match }) => {
   const [getSubmissions, submissionsLoading] = useApi(API.USERS.SUBMISSIONS);
-  const [getUser] = useApi(API.USERS.SHOW);
+  const [getUser, userLoading] = useApi(API.USERS.SHOW);
   const [submissions, setSubmissions] = useState([]);
   const [user, setUser] = useState(null);
 
   const [__, loggedInUser] = useContext(LoginContext);
   const [lastEvent] = useContext(EventContext);
+  const [theme] = useContext(ThemeContext);
 
   useEffect(() => {
     if (lastEvent
@@ -30,6 +35,17 @@ const Profile = ({ match }) => {
     getSubmissions(match.params.id).then(res => setSubmissions(res.submissions));
     getUser(match.params.id).then(res => setUser(res.user));
   }, [match.params.id]);
+
+  if (!user && !userLoading) {
+    return (
+      <div className={cn(styles.errorContainer, styles[theme])}>
+        <div>
+          <img src={crossIcon} draggable='false' alt='Error' />
+        </div>
+        <div>{'This user doesn\'t exist.'}</div>
+      </div>
+    );
+  }
 
   return (
     <>
