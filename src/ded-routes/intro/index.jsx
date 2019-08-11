84f01b1aa8ea9323f4d7d4bec4be66a079b4a834
warau0 <larswalwes@gmail.com>
2019-08-11 18:26:11 +0200

    Intro thoughts

--- src/ded-routes/intro/index.jsx
+++ src/ded-routes/intro/index.jsx
@@ -4,6 +4,46 @@ import Layout from 'ded-components/layout';
 
 export default () => (
   <Layout>
-    Intro page, nothing here yet. Log in to start posting.
+    <h2>What is this, and why should I care?</h2>
+    <p>If you want to get better. Not for gathering a following.</p>
+    <p>Whether you're pursing a career in art or you're a passionate hobbyist.</p>
+    <p>Group of peers</p>
+
+    <h3>I need some motivation, is just drawing enough?</h3>
+    <p>While mileage is important, eternal aimless doodling won't help you improve.</p>
+    <p>Don't burn yourself out, draw what you want to draw and supplement with studies when you feel stuck.</p>
+
+    {/* FZD Just draw episode */}
+
+    <p>Define long and short term goals and meassure your progress. How are you working towards achieving your goals?</p>
+    <p>A goal can be as simple and concrete as drawing a walk cycle, completing a master study or hitting 100 followers on a platform.</p>
+    <p>You want a goal, but you don't want the distance between you and the goal to crush you.</p>
+    <p>Break down your overarching goals into smaller pieces so you have a fairly high likelyhood of achieving them.</p>
+    <p>Or as loose as eclipsing your art nemesis. Be wary of goals you can't meassure or are too ambitious.</p>
+
+    {/* Video on setting goals */}
+
+    <p>Don't compare yourself to others too much. There's always going to other people who are better than you, and those who are worse.</p>
+    <p>Everyone is at a different point in their art journey relative to you. You don't know the details of other peoples lives.</p>
+    <p>Compare yourself to your past self. Small incremental improvements.</p>
+    <p>It's pointless to anguish yourself with the thought of 12 year old prodigies in China who are years ahead of where you are at 25.</p>
+    <p>It's good to have artists you look up to and aim towards, but be careful not be wallow in hopelessness and become resentfull.</p>
+
+    {/* Jake the dog video */}
+
+    <h3>Alright, I'm in, what do I do?</h3>
+    <p>Make an account and start uploading your art every day. You should spend at least 30 minutes on what you upload.</p>
+    <p>If you want to get fancy with it start recording how long you spend on art.</p>
+
+    <h3>I'm kind of a busy guy, can I post less frequently than every day?</h3>
+    <p>No. I don't care if you're the CEO of a multi million dollar corporation. You can carve out 30 minutes of your day to draw something if you want to.</p>
+    <p>If you don't find any time to draw at least that little, it's because you don't want to or don't give yourself the chance to.</p>
+    <p>Maybe you're on your phone on the bus, maybe you aimlessly click around on YouTube or Facebook for an hour after dinner.</p>
+    <p>Start bringing your sketchbook around with you. If you have it, you have an alternative to wasting time on your phone when you have some dead time.</p>
+
+    <p>Don't burn yourself out.</p>
+    <p>As long as you're drawing, you're doing good, champ.</p>
+
+    {/* Japanese crabber video */}
   </Layout>
 );
